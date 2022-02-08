b4948eef24c121fb4fe1b7731c29f7f410c0a05b
AmarHAsecic <ahasecic1@etf.unsa.ba>
2022-02-08 14:18:01 +0100

    database added.

---
+++ baza.sql
@@ -0,0 +1,48 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "CECMembers" (
+	"code"	TEXT,
+	"username"	TEXT,
+	"first_name"	TEXT,
+	"last_name"	TEXT,
+	PRIMARY KEY("code")
+);
+CREATE TABLE IF NOT EXISTS "Candidates" (
+	"id"	INTEGER,
+	"first_name"	TEXT,
+	"last_name"	TEXT,
+	PRIMARY KEY("id")
+);
+CREATE TABLE IF NOT EXISTS "Members" (
+	"idMember"	INTEGER,
+	"first_name"	TEXT,
+	"last_name"	TEXT,
+	"jmbg"	TEXT,
+	"date_of_birth"	TEXT,
+	"email"	TEXT,
+	"phone"	TEXT,
+	PRIMARY KEY("idMember")
+);
+CREATE TABLE IF NOT EXISTS "PoliticalParties" (
+	"id"	INTEGER,
+	"name"	TEXT,
+	"president"	INTEGER,
+	"member"	INTEGER,
+	FOREIGN KEY("president") REFERENCES "Members",
+	FOREIGN KEY("member") REFERENCES "Members",
+	PRIMARY KEY("id")
+);
+CREATE TABLE IF NOT EXISTS "Voters" (
+	"id_pass"	TEXT,
+	"first_name"	TEXT,
+	"last_name"	TEXT,
+	"username"	TEXT,
+	"jmbg"	TEXT,
+	"date_of_birth"	TEXT,
+	"city"	TEXT,
+	"adress"	TEXT,
+	"email"	TEXT,
+	"phone"	TEXT,
+	PRIMARY KEY("id_pass")
+);
+INSERT INTO "Voters" VALUES ('amar','Amar','Hasečić','amar','180500070005','18.5.2000','Sarajevo','Topal Osman Paše 12','ahasecic1@etf.unsa.ba','603347350');
+COMMIT;
--- src/ba/unsa/etf/rpr/MainController.java
+++ src/ba/unsa/etf/rpr/MainController.java
@@ -4,12 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ResourceBundle;
 
 public class MainController{
 
