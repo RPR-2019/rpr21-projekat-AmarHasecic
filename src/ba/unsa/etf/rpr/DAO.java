1e6080f7801bf4ac28bbb1f1956591bdd7be4156
AmarHAsecic <ahasecic1@etf.unsa.ba>
2022-02-08 13:11:52 +0100

    Some code for handeling database, DAO Class edited.

--- src/ba/unsa/etf/rpr/DAO.java
+++ src/ba/unsa/etf/rpr/DAO.java
@@ -1,4 +1,87 @@
 package ba.unsa.etf.rpr;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class DAO {
+
+    private Connection conn;
+    private static DAO instance;
+
+    public DAO() {
+        String url = "jdbc:sqlite:baza.db";
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DAO getInstance() {
+        if (instance == null) instance = new DAO();
+        return instance;
+    }
+
+    public static void removeInstance() {
+        if (instance == null) return;
+        instance.close();
+        instance = null;
+    }
+
+    public void close() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createDatabase() {
+        Scanner ulaz = null;
+        try {
+            ulaz = new Scanner(new FileInputStream("baza.db.sql"));
+            String sqlUpit = "";
+            while (ulaz.hasNext()) {
+                sqlUpit += ulaz.nextLine();
+                if (sqlUpit.charAt(sqlUpit.length() - 1) == ';') {
+                    try {
+                        Statement stmt = conn.createStatement();
+                        stmt.execute(sqlUpit);
+                        sqlUpit = "";
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            ulaz.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void setDefaultDatabase() {
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            stmt.executeUpdate("DELETE FROM grad");
+            stmt.executeUpdate("DELETE FROM drzava");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        createDatabase();
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
 }
--- src/ba/unsa/etf/rpr/MainController.java
+++ src/ba/unsa/etf/rpr/MainController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
