7bb03b8fa7a950429bfccb24f93d94182a5924bb
AmarHAsecic <ahasecic1@etf.unsa.ba>
2022-02-15 13:04:47 +0100

    Allerts added for returning to factory settings.

--- src/ba/unsa/etf/rpr/FactorySettingsController.java
+++ src/ba/unsa/etf/rpr/FactorySettingsController.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 
 public class FactorySettingsController {
 
@@ -8,10 +9,18 @@ public class FactorySettingsController {
 
 public void testSettingsAction(ActionEvent actionEvent){
     model.setDefaultDatabaseTest();
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setHeaderText("Database test mode on.");
+    alert.setContentText("Log in again to see changes.");
+    alert.showAndWait();
 }
 
 public void deleteDataAction(ActionEvent actionEvent){
     model.setDefaultDatabase();
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setHeaderText("Database default mode on.");
+    alert.setContentText("Log in again to see changes.");
+    alert.showAndWait();
 }
 
 }
