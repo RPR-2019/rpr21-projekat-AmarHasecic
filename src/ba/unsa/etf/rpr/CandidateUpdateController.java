f3e2d22cefcffa5d3577392622105c63405e97e2
AmarHAsecic <ahasecic1@etf.unsa.ba>
2022-02-17 12:50:27 +0100

    Data validation added for candidate.

--- src/ba/unsa/etf/rpr/CandidateUpdateController.java
+++ src/ba/unsa/etf/rpr/CandidateUpdateController.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -39,18 +40,59 @@ public class CandidateUpdateController {
         }
 
 
+        firstName.textProperty().addListener((obs, oldIme, newIme) -> {
+            if (!newIme.isEmpty()) {
+                firstName.getStyleClass().removeAll("poljeNijeIspravno");
+                firstName.getStyleClass().add("poljeIspravno");
+            } else {
+                firstName.getStyleClass().removeAll("poljeIspravno");
+                firstName.getStyleClass().add("poljeNijeIspravno");
+            }
+        });
+
+        lastName.textProperty().addListener((obs, oldIme, newIme) -> {
+            if (!newIme.isEmpty()) {
+                lastName.getStyleClass().removeAll("poljeNijeIspravno");
+                lastName.getStyleClass().add("poljeIspravno");
+            } else {
+                lastName.getStyleClass().removeAll("poljeIspravno");
+                lastName.getStyleClass().add("poljeNijeIspravno");
+            }
+        });
+
+        party.textProperty().addListener((obs, oldIme, newIme) -> {
+            if (!newIme.isEmpty()) {
+                party.getStyleClass().removeAll("poljeNijeIspravno");
+                party.getStyleClass().add("poljeIspravno");
+            } else {
+                party.getStyleClass().removeAll("poljeIspravno");
+                party.getStyleClass().add("poljeNijeIspravno");
+            }
+        });
+
     }
     public void okAction(ActionEvent actionEvent){
-        if (candidate == null) {
-            candidate = new Candidate();
+
+        if(firstName.getText().isBlank() || lastName.getText().isBlank() || party.getText().isBlank())
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Field empty");
+            alert.setContentText("You have to fill every field in this form");
+            alert.showAndWait();
         }
+        else {
 
-        candidate.setFirstName(firstName.getText());
-        candidate.setLastName(lastName.getText());
-        candidate.setPoliticalPartyId(Integer.parseInt(party.getText()));
+            if (candidate == null) {
+                candidate = new Candidate();
+            }
 
-        Stage stage = (Stage) firstName.getScene().getWindow();
-        stage.close();
+            candidate.setFirstName(firstName.getText());
+            candidate.setLastName(lastName.getText());
+            candidate.setPoliticalPartyId(Integer.parseInt(party.getText()));
+
+            Stage stage = (Stage) firstName.getScene().getWindow();
+            stage.close();
+        }
 
     }
 
