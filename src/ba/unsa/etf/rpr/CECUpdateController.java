5584b14efedfaf0b96fb9c6d435fc4bbfbafac85
AmarHAsecic <ahasecic1@etf.unsa.ba>
2022-02-17 16:58:31 +0100

    Data validation added for cec.

--- src/ba/unsa/etf/rpr/CECUpdateController.java
+++ src/ba/unsa/etf/rpr/CECUpdateController.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -37,19 +38,59 @@ public class CECUpdateController {
             lastName.setText(member.getLastName());
         }
 
+        firstName.textProperty().addListener((obs, oldIme, newIme) -> {
+            if (!newIme.isEmpty()) {
+                firstName.getStyleClass().removeAll("poljeNijeIspravno");
+                firstName.getStyleClass().add("poljeIspravno");
+            } else {
+                firstName.getStyleClass().removeAll("poljeIspravno");
+                firstName.getStyleClass().add("poljeNijeIspravno");
+            }
+        });
+
+        lastName.textProperty().addListener((obs, oldIme, newIme) -> {
+            if (!newIme.isEmpty()) {
+                lastName.getStyleClass().removeAll("poljeNijeIspravno");
+                lastName.getStyleClass().add("poljeIspravno");
+            } else {
+                lastName.getStyleClass().removeAll("poljeIspravno");
+                lastName.getStyleClass().add("poljeNijeIspravno");
+            }
+        });
+
+        usernameCEC.textProperty().addListener((obs, oldIme, newIme) -> {
+            if (!newIme.isEmpty()) {
+                usernameCEC.getStyleClass().removeAll("poljeNijeIspravno");
+                usernameCEC.getStyleClass().add("poljeIspravno");
+            } else {
+                usernameCEC.getStyleClass().removeAll("poljeIspravno");
+                usernameCEC.getStyleClass().add("poljeNijeIspravno");
+            }
+        });
+
     }
 
     public void okAction(ActionEvent actionEvent){
-        if (member == null) {
-            member = new CECMember();
-
+        if(firstName.getText().isBlank() || lastName.getText().isBlank() || usernameCEC.getText().isBlank())
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Field empty");
+            alert.setContentText("You have to fill every field in this form");
+            alert.showAndWait();
         }
-        member.setUsername(usernameCEC.getText());
-        member.setFirstName(firstName.getText());
-        member.setLastName(lastName.getText());
+        else {
 
-        Stage stage = (Stage) usernameCEC.getScene().getWindow();
-        stage.close();
+            if (member == null) {
+                member = new CECMember();
+
+            }
+            member.setUsername(usernameCEC.getText());
+            member.setFirstName(firstName.getText());
+            member.setLastName(lastName.getText());
+
+            Stage stage = (Stage) usernameCEC.getScene().getWindow();
+            stage.close();
+        }
 
     }
 
